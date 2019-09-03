cc4403e16faec3acf0080fa6a8c5af2fde445fe2
Kronos <desarrollador.grupoempresarial@gmail.com>
2019-09-03 16:23:38 -0500

    Diseño en el login

--- application/views/frontend/auth/login.php
+++ application/views/frontend/auth/login.php
@@ -1,8 +1,8 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
-<div class="container">
-  <div class="col-md-4 center-block no-float">
+<br>
+<div class="container text-center col-md-4">
     <div class="panel panel-primary">
         <div class="panel-heading">
           <h3 class="panel-title"><?php echo $panel_title; ?></h3>
@@ -11,20 +11,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
           <?php if (validation_errors()) {echo '<div class="alert alert-danger" role="alert">' . validation_errors() . '</div>';}?>
 
           <?php echo form_open('Signin'); ?>
-            <div class="form-group">
+            <div class="form-group text-left">
               <label for="exampleInputEmail1">Correo electronico</label>
               <input type="email" class="form-control" name="correo" id="correo" placeholder="Correo">
             </div>
-            <div class="form-group">
+            <div class="form-group text-left">
               <label for="exampleInputPassword1">Contraseña</label>
               <input type="password" class="form-control" name="pass" id="pass" placeholder="Password">
             </div>
-            <button type="submit" class="btn btn-default">Entrar</button>
+            <button type="submit" class="btn btn-success col-md-12">Entrar</button>
         </form>
       </div>
     </div>
-  </div>
 </div>
-<div class="panel panel-primary">
-  Prueba
-</div>
+
